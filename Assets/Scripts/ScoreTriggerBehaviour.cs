46a6c7c38de72f17b73a8a5d1b00802255c4a2ad
Brett Stelly <bstelly1999@gmail.com>
2024-10-24 16:16:07 -0500

    Added some extra checks for scoring

--- Assets/Scripts/ScoreTriggerBehaviour.cs
+++ Assets/Scripts/ScoreTriggerBehaviour.cs
@@ -11,6 +11,7 @@ public class ScoreTriggerBehaviour : MonoBehaviour
     private Vector3 enteredVelocity;
     private Vector3 exitVelocity;
     private string lastPlayerContactOnBall;
+    private bool enteredFromAbove;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -18,6 +19,10 @@ public class ScoreTriggerBehaviour : MonoBehaviour
         {
             enteredVelocity = BallRigidBody.velocity;
             lastPlayerContactOnBall = other.GetComponent<GrabbedBehavior>().LastPlayerTag;
+            if(other.transform.position.y > transform.position.y)
+            {
+                enteredFromAbove = true;
+            }
         }
     }
     private void OnTriggerExit(Collider other)
@@ -26,9 +31,11 @@ public class ScoreTriggerBehaviour : MonoBehaviour
         {
             exitVelocity = BallRigidBody.velocity;
         }
-        if(enteredVelocity.y < 0 && exitVelocity.y < 0) //Ball was moving downwards when entering and exiting the trigger
+        if(enteredVelocity.y < 0 && exitVelocity.y < 0 && enteredFromAbove) //Ball was moving downwards when entering and exiting the trigger
         {
+            enteredFromAbove = false;
             ScoreBoard.PlayerScored(lastPlayerContactOnBall);
+            other.GetComponent<GrabbedBehavior>().LastPlayerTag = "";
         }
     }
 }
