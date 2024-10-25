8620e93051e4fa5b4c4620d87e5fd362eb2667ff
Brett Stelly <bstelly1999@gmail.com>
2024-10-24 19:45:14 -0500

    Added ability for players to get an extra point for having a streak

--- Assets/Scripts/ScoreBoardBehaviour.cs
+++ Assets/Scripts/ScoreBoardBehaviour.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using TMPro;
 using UnityEngine;
 
@@ -11,6 +12,9 @@ public class ScoreBoardBehaviour : MonoBehaviour
     private int playerOneScore;
     private int playerTwoScore;
 
+    private bool PlayerOneOnAStreak;
+    private bool PlayerTwoOnAStreak;
+
     void Update()
     {
         PlayerOneScoreText.text = playerOneScore.ToString();
@@ -21,11 +25,23 @@ public class ScoreBoardBehaviour : MonoBehaviour
     {
         if(tag == "Player1")
         {
+            PlayerTwoOnAStreak = false;
+            if(PlayerOneOnAStreak)
+            {
+                playerOneScore += 2;
+            }
             playerOneScore += 1;
+            PlayerOneOnAStreak = true;
         }
         else if(tag == "Player2")
         {
+            PlayerOneOnAStreak = false;
+            if(PlayerTwoOnAStreak)
+            {
+                playerTwoScore += 2;
+            }
             playerTwoScore += 1;
+            PlayerTwoOnAStreak = true;
         }
     }
 }
